e0a40ce469589297b27eb6184fb19ba4d4e0597f
Woodson <woodsondelhia@hotmail.com>
2014-11-01 02:03:41 -0400

    Log in/ log out not finish almost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
    def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
   		flash[:success] = "Welcome to AC Shift Trade Centre!" 
   		redirect_to @user
   	else
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,4 +12,28 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
  		end
  		assert_template 'users/show'
 	end
+
+	test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Example",
+      										username: "Hell1111",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
+  
 end
